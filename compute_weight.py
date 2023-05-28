76f69eb00250534ef4479916fd99820fc22696da
true_mogician <truemogician@gmail.com>
2023-05-28 10:56:15 +0800

    feat: add command line args support for compute_weight

--- compute_weight.py
+++ compute_weight.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import sys
+
 import xlwt
 from preprocess_data import *
 from connectivity.PMI import *
@@ -27,7 +29,10 @@ def compute_weight(data_path: str, out_path: str):
 
 
 if __name__ == '__main__':
-    files = get_data_files()
-    for [i, f] in files.items():
+    data_files = get_data_files()
+    if len(sys.argv) > 1:
+        indices = [int(i) for i in sys.argv[1:]]
+        data_files = {k: v for k, v in data_files.items() if k in indices}
+    for [i, f] in data_files.items():
         print(f"Processing {i}...")
         compute_weight(f, f"result/sub-{i:02}/eeg_initial_weight.xls")
