f5f7bb2b67238b977807d1751d373406c0ecb98a
true_mogician <truemogician@gmail.com>
2023-06-03 12:58:09 +0800

    feat: make dict_to_namedtuple more robust
    NamedTuple is changed into a function from class, thus I have to use some other ways to check whether a type is derived from it;
    Support NamedTuple as generic argument of Dict and List.

--- src/utils/common.py
+++ src/utils/common.py
@@ -16,21 +16,62 @@ def ensure_dir(path: os.PathLike):
     Path(path).parent.mkdir(parents=True, exist_ok=True)
     return path
 
+def is_namedtuple(tp: Type):
+    if not (isinstance(tp, type) and issubclass(tp, tuple)):
+        return False
+    if not (hasattr(tp, "__annotations__") and hasattr(tp, "_fields") and hasattr(tp, "_field_defaults")):
+        return False
+    if not (isinstance(tp.__annotations__, dict) and isinstance(tp._fields, tuple) and isinstance(tp._field_defaults, dict)):
+        return False
+    return set(tp.__annotations__.keys()) == set(tp._fields)
+
 T = TypeVar("T", bound=NamedTuple)
 def dict_to_namedtuple(data: Dict[str, object], nt_class: Type[T]) -> T:
+    def _convert_dict(value: Dict[str, Any], target_type: Type):
+        if is_namedtuple(target_type):
+            return dict_to_namedtuple(value, target_type)
+        elif hasattr(target_type, "__origin__") and target_type.__origin__ is dict:
+            args = target_type.__args__
+            assert len(args) == 2
+            value_type = args[1]
+            if is_namedtuple(value_type):
+                return {k: dict_to_namedtuple(v, value_type) for k, v in value.items()}
+            elif hasattr(value_type, "__origin__"):
+                origin = value_type.__origin__
+                if origin is dict:
+                    return {k: _convert_dict(v, value_type) for k, v in value.items()}
+                elif origin is list:
+                    return {k: _convert_list(v, value_type) for k, v in value.items()}
+        return value
+    
+    def _convert_list(value: List[Any], target_type: Type[List]):
+        assert hasattr(target_type, "__origin__") and target_type.__origin__ is list
+        args = target_type.__args__
+        assert len(args) == 1
+        value_type = args[0]
+        if is_namedtuple(value_type):
+            return [dict_to_namedtuple(v, value_type) for v in value]
+        elif hasattr(value_type, "__origin__"):
+            origin = value_type.__origin__
+            if origin is dict:
+                return [_convert_dict(v, value_type) for v in value]
+            elif origin is list:
+                return [_convert_list(v, value_type) for v in value]
+        return value
+    
     fields = nt_class._fields
     field_types = nt_class.__annotations__
     values = []
     for field in fields:
-        if field in data:
-            value = data[field]
-            if isinstance(value, dict) and field in field_types:
-                nested_nt = dict_to_namedtuple(value, field_types[field])
-                values.append(nested_nt)
-            else:
-                values.append(value)
-        else:
-            values.append(None)
+        new_value = None
+        if field in data and field in field_types:
+            new_value = value = data[field]
+            field_type = field_types[field]
+            if isinstance(value, dict):
+                new_value = _convert_dict(value, field_type)
+            elif isinstance(value, list):
+                new_value = _convert_list(value, field_type)
+        values.append(new_value)
     return nt_class(*values) # type: ignore
 
 def save_to_sheet(
